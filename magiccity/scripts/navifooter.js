0e1296433256f0091c7996393ad4d1dcd33ddc3a
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-04-23 23:28:50 -0400

    Wahahahaha

--- magiccity/scripts/navifooter.js
+++ magiccity/scripts/navifooter.js
@@ -3,30 +3,14 @@ document.getElementById("foot01").innerHTML =
 
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/index2.php'>Home</a></li>" +
-"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/about.php'>About</a></li>" +
-"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/art.php'>MC Art Museum</a></li>" +
-"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/movies.php'>MC Theater</a></li>" +
-"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/games.php'>MC Arcade</a></li>" +
-"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/flash.php'>Flash</a></li>" +
-"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/downloads.php'>Downloads</a></li>" +
-"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/other.php'>Other Stuff</a></li>" +
-"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/pokeevents.php'>Pok&#233;mon QR Events</a></li>" +
+"<li><a href='index2.html'>Home</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='art.html'>MC Art Museum</a></li>" +
+"<li><a href='movies.html'>MC Theater</a></li>" +
+"<li><a href='games.html'>MC Arcade</a></li>" +
+"<li><a href='flash.html'>Flash</a></li>" +
+"<li><a href='downloads.html'>Downloads</a></li>" +
+"<li><a href='other.html'>Other Stuff</a></li>" +
+"<li><a href='pokeevents.html'>Pok&#233;mon QR Events</a></li>" +
 "</ul>";
 }
-
-/*
-     FILE ARCHIVED ON 21:38:01 Nov 03, 2015 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 01:20:46 Apr 24, 2026.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  capture_cache.get: 1.89
-  load_resource: 181.847
-  PetaboxLoader3.resolve: 58.484
-  PetaboxLoader3.datanode: 49.487
-*/
--- magiccity/scripts/tinkerbell.js
+++ magiccity/scripts/tinkerbell.js
@@ -1,12 +1,3 @@
-var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
-let window = _____WB$wombat$assign$function_____("window");
-let self = _____WB$wombat$assign$function_____("self");
-let document = _____WB$wombat$assign$function_____("document");
-let location = _____WB$wombat$assign$function_____("location");
-let top = _____WB$wombat$assign$function_____("top");
-let parent = _____WB$wombat$assign$function_____("parent");
-let frames = _____WB$wombat$assign$function_____("frames");
-let opens = _____WB$wombat$assign$function_____("opens");
 // <![CDATA[
 var colour="random"; // in addition to "random" can be set to any valid colour eg "#f0f" or "red"
 var sparkles=50;
@@ -204,19 +195,3 @@ function newColour() {
 }
 // ]]>
 }
-
-/*
-     FILE ARCHIVED ON 21:37:58 Nov 03, 2015 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 01:20:44 Apr 24, 2026.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  capture_cache.get: 0.4
-  load_resource: 54.587
-  PetaboxLoader3.resolve: 33.239
-  PetaboxLoader3.datanode: 20.134
-*/
