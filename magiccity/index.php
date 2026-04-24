0eefcd1741e1b1aaed51590188d7f96410ccdb33
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-04-23 22:23:29 -0400

    Added Magic City

---
+++ magiccity/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>ENTRANCE</title>
+<style type="text/css">
+body {
+color: white;
+background-color: black;
+font-style: normal;
+text-align: center;
+font-family: Arial,Helvetica,sans-serif;
+}
+</style></head>
+<body>
+<center><big>
+<p>Click a picture of Himitsu on the left to Enter. Otherwise, just click Aqua on the right to leave this website.</p>
+</big><a href="index2.php"><big><img style="border: 0px solid ; width: 169px; height: 195px;" alt="Himitsu will make a tour about Magic City there." src="images/himitsu2.png"></big></a><a href="https://gaiawindwave90.github.io/"><big><img style="border: 0px solid ; width: 123px; height: 145px;" alt="Even Aqua didn't make you leave, but instead, you'll go into a surprise place!" src="images/Aqua%20Mew.png"></big></a><br><br>
+<big>Disclaimer: I do own all my original characters, but I do
+not own any of companies I like such as Nintendo or Capcom.</big>
+</center>
+</body></html>
+
+
---
+++ magiccity/scripts/navifooter.js
@@ -0,0 +1,32 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy; " + new Date().getFullYear() + " Magic City.<br>Powered by <a href='https://github.com/'>GitHub.Com</a>.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/index2.php'>Home</a></li>" +
+"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/about.php'>About</a></li>" +
+"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/art.php'>MC Art Museum</a></li>" +
+"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/movies.php'>MC Theater</a></li>" +
+"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/games.php'>MC Arcade</a></li>" +
+"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/flash.php'>Flash</a></li>" +
+"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/downloads.php'>Downloads</a></li>" +
+"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/other.php'>Other Stuff</a></li>" +
+"<li><a href='https://web.archive.org/web/20151103213801/http://magiccity.netii.net/pokeevents.php'>Pok&#233;mon QR Events</a></li>" +
+"</ul>";
+}
+
+/*
+     FILE ARCHIVED ON 21:38:01 Nov 03, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 01:20:46 Apr 24, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  capture_cache.get: 1.89
+  load_resource: 181.847
+  PetaboxLoader3.resolve: 58.484
+  PetaboxLoader3.datanode: 49.487
+*/
